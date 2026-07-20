76a15e84620b259a2575d2fa3d95c493d2805e22
Fahim36-sys <fahimhasnat32@outlook.com>
2026-07-20 13:36:57 +0600

    Lua code successfully tested

--- src/server/math.server.lua
+++ src/server/math.server.lua
@@ -1,6 +1,15 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Workspace = game:GetService("Workspace")
 function add(a, b)
     return a + b
 end
 local result = add(5, 3)
 print("Sum: " .. result)
 print("Welcome to new roblox game!")
+local obj=Workspace.Baseplate.part
+obj.Anchored=true
+obj.Color=Color3.fromRGB(255, 0, 0)
+--if touched obj, color will change to blue
+obj.Touched:Connect(function(hit)
+    obj.Color=Color3.fromRGB(0, 0, 255)
+end)
